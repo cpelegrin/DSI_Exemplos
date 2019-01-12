763c05f21b24ef32197d627b36eda90317198a15
Carlos Pelegrin <carloseduardopelegrin@gmail.com>
2019-01-12 16:07:32 -0200

    ADD exemplo 1, arquivos de testes e resultados

---
+++ exemplo1/exemploxml.php
@@ -0,0 +1,16 @@
+<?php
+// O arquivo test.xml contém um documento XML
+if (file_exists('teste.xml')) { //se o arquivo existir na pasta especificada
+    $xml = simplexml_load_file('teste.xml');//Faz a leitura do arquivo e o converte em um objeto
+
+    print_r($xml);//imprime a estrutura do objeto
+} else {
+    exit('Falha ao abrir teste.xml.');
+}
+
+	$xml->idade = 30;//altera o valor do arquivo como um objeto
+	echo "<br>Idade: $xml->idade";//somente para conferência, imprime o valor da chave idade
+
+	$novoXml = fopen("xml_corrigido.xml", "w") or die("Impossível ler o arquivo");//cria novo arquivo
+	fwrite($novoXml, $xml->asXML());//salva o novo arquivo em formato xml
+?>
