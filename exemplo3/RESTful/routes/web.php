4ab282d36639113285f8302631531ee7fb59f5c9
Carlos Pelegrin <carloseduardopelegrin@gmail.com>
2019-03-06 20:21:30 -0300

    ADD exemplo de RESTful

---
+++ exemplo3/RESTful/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return 'Olá Lumen';
+});
+
+
+$router->get('populatedb', 'ExampleController@populateDB');
+
+$router->get('get_names', 'ExampleController@get_names');
+
+$router->get('get_names_and_ages', 'ExampleController@get_names_and_ages');
+
+$router->get('get_age_by_name/{name}', 'ExampleController@get_age_by_name');
