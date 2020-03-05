fd2664ab8aadcf3ea7c878c88272ac403ec9234d
Carlos Pelegrin <carloseduardopelegrin@gmail.com>
2020-03-05 14:06:01 -0300

    FIX melhoria de mensagens

--- rest_exemplo/client.php
+++ rest_exemplo/client.php
@@ -2,10 +2,12 @@
 $curl = curl_init();
 
 $url = "http://localhost/DSI_Exemplos/rest_exemplo/rest_server.php";
+
 //POST
 curl_setopt($curl, CURLOPT_POST, 1);
 $data = array("param" => "value"); 
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+//end POST
 
 curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
@@ -13,7 +15,7 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $result = curl_exec($curl);
 
 $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-echo 'HTTP Status Code: ' . $httpcode;
+echo 'HTTP Status Code: ' . $httpcode . "<br>";
 
 curl_close($curl);
 print_r($result);
--- rest_exemplo/rest_server.php
+++ rest_exemplo/rest_server.php
@@ -1,10 +1,9 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST") {
 	if(count($_POST)>0) {
-		print "<PRE>";
 		http_response_code(503);
+		print("Retorno de um método POST: ");
 		print_r($_POST);
-		print "</PRE>";
 	}
 }
 if($_SERVER["REQUEST_METHOD"] == "GET") {
@@ -12,8 +11,8 @@ if($_SERVER["REQUEST_METHOD"] == "GET") {
 		print("nome: ".$_GET['nome']."<br>");
 
 	}
-	print("Isso aqui é um GET");
+	print("Retorno de um método GET");
 	//header("Content-Type: application/json; charset=UTF-8");
-	//echo json_encode(array("resposta" => "Isso aqui é um método GET"));
+	//echo json_encode(array("resposta" => "Retorno de um método GET"));
 }
 ?>
