dbfc149975b8a31f6b520f15b93182ad9208471c
Carlos Pelegrin <carloseduardopelegrin@gmail.com>
2020-03-09 10:57:36 -0300

    incluindo exemplo lumen

---
+++ rest_lumen/app/Http/Controllers/ExampleController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+class Pessoas extends Model{}
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function populateDB(){
+        $pessoas = new Pessoas;
+        $pessoas->nome = 'carlos';
+        $pessoas->idade = 30;
+        $pessoas->sexo = 'M';
+        $pessoas->estado_civil = 'solteiro';
+        $pessoas->endereco = 'rua B no 01';
+        $pessoas->usuario = 'cpelegrin';
+        $pessoas->senha = sha1(123456);
+        $pessoas->save();
+
+        $pessoas = new Pessoas;
+        $pessoas->nome = 'jose';
+        $pessoas->idade = 50;
+        $pessoas->sexo = 'M';
+        $pessoas->estado_civil = 'casado';
+        $pessoas->endereco = 'rua B no 02';
+        $pessoas->usuario = 'jose_vieira';
+        $pessoas->senha = sha1(456789);
+        $pessoas->save();
+
+        $pessoas = new Pessoas;
+        $pessoas->nome = 'maria';
+        $pessoas->idade = 35;
+        $pessoas->sexo = 'F';
+        $pessoas->estado_civil = 'casada';
+        $pessoas->endereco = 'rua C no 01';
+        $pessoas->usuario = 'maria';
+        $pessoas->senha = sha1(987654);
+        $pessoas->save();
+
+        $pessoas = new Pessoas;
+        $pessoas->nome = 'camila';
+        $pessoas->idade = 20;
+        $pessoas->sexo = 'F';
+        $pessoas->estado_civil = 'solteira';
+        $pessoas->endereco = 'rua C no 01';
+        $pessoas->usuario = 'mila123';
+        $pessoas->senha = sha1(123789);
+        $pessoas->save();
+
+        return "Dados inseridos no Banco";
+
+    }
+
+    public function get_names(){
+        return $results = DB::select("SELECT nome FROM pessoas");
+    }
+
+    public function get_names_and_ages(){
+        return $results = DB::select("SELECT nome, idade FROM pessoas");
+    }
+
+    public function get_age_by_name($name){
+        return $results = DB::select("SELECT idade FROM pessoas WHERE nome='$name'");
+    }
+}
---
+++ rest_lumen/database/migrations/2020_03_09_130514_create_pessoas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePessoasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pessoas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome');
+            $table->integer('idade');
+            $table->string('sexo');
+            $table->string('estado_civil');
+            $table->string('endereco');
+            $table->string('usuario');
+            $table->string('senha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pessoas');
+    }
+}
